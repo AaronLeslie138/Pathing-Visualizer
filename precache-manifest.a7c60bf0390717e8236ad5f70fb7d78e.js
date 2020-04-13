8130f9de92f6db7250da210262ebf3a5809df70d
Aaron Leslie <aaronleslie138@gmail.com>
2020-04-12 23:06:21 -0400

    Updates

---
+++ precache-manifest.a7c60bf0390717e8236ad5f70fb7d78e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96802ca4156c0d05cf29da1a30842fc5",
+    "url": "/Pathing-Visualizer/index.html"
+  },
+  {
+    "revision": "0e87c0b7aad4a2b90dad",
+    "url": "/Pathing-Visualizer/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4a016842eeb33efdef2f",
+    "url": "/Pathing-Visualizer/static/css/main.fb398a19.chunk.css"
+  },
+  {
+    "revision": "0e87c0b7aad4a2b90dad",
+    "url": "/Pathing-Visualizer/static/js/2.1693f27e.chunk.js"
+  },
+  {
+    "revision": "95fe7b47190d8ffb46ace11b00922862",
+    "url": "/Pathing-Visualizer/static/js/2.1693f27e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a016842eeb33efdef2f",
+    "url": "/Pathing-Visualizer/static/js/main.dcb293f2.chunk.js"
+  },
+  {
+    "revision": "f4ee4377a610b058464c",
+    "url": "/Pathing-Visualizer/static/js/runtime-main.d9b76f5e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pathing-Visualizer/precache-manifest.7a8c7a06dfa474d941f1ab0dffe4a8a5.js"
+  "/Pathing-Visualizer/precache-manifest.a7c60bf0390717e8236ad5f70fb7d78e.js"
 );
 
 self.addEventListener('message', (event) => {
